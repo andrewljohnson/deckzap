ee5b66ec4d013cb205b9ddcc0da0c38148378cc3
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro-2.local>
2021-04-01 21:53:43 -0700

    fix daphne/asgi

--- demogames/asgi.py
+++ demogames/asgi.py
@@ -1,12 +1,13 @@
 import os
+import django
 
+django.setup()
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import cofx.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demogames.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
