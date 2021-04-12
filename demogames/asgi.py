9abe357ad71e0aedfb713f58fa1736f92cb633ff
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro-2.local>
2021-04-12 14:58:09 -0700

    tweak asgi

--- demogames/asgi.py
+++ demogames/asgi.py
@@ -1,13 +1,13 @@
 import os
 import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demogames.settings')
 django.setup()
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import cofx.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demogames.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
