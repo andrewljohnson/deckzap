098626f8fb6f56f8cc33f09b1894501f6bd8358b
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro-2.local>
2021-04-18 23:33:00 -0700

    fix tests

--- battle_wizard/tests.py
+++ battle_wizard/tests.py
@@ -9,14 +9,14 @@ class GameObjectTests(TestCase):
 
     def game_with_two_players(self):
         game_dict = JsonDB().game_database(TEST_DB_NAME)
-        game = Game("ingame", info=game_dict)        
-        game.play_move({"username": "a", "move_type": "JOIN"}, TEST_DB_NAME)
-        game.play_move({"username": "b", "move_type": "JOIN"}, TEST_DB_NAME)
+        game = Game(None, TEST_DB_NAME, "ingame", info=game_dict)        
+        game.play_move({"username": "a", "move_type": "JOIN", "log_lines":[]})
+        game.play_move({"username": "b", "move_type": "JOIN", "log_lines":[]})
         return game
 
     def test_new_ingame_game(self):
         game_dict = JsonDB().game_database(TEST_DB_NAME)
-        game = Game("ingame", info=game_dict)        
+        game = Game(None, TEST_DB_NAME, "ingame", info=game_dict)        
         self.assertEqual(game.turn, 0)
 
     def test_two_players_join_ingame_game(self):
@@ -28,7 +28,6 @@ class GameObjectTests(TestCase):
 
     def test_illegal_opponent_start_turn(self):
         game = self.game_with_two_players()
-        game.play_move({"username": "b", "move_type": "START_TURN"}, TEST_DB_NAME)
+        game.play_move({"username": "b", "move_type": "START_TURN", "log_lines":[]})
         self.assertEqual(len(game.opponent().hand), 2)
         os.remove(f"database/games/{TEST_DB_NAME}.json")
-
