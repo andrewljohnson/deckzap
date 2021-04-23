5a30783e4ee9394c922080b737ae473e03ae76ee
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro-2.local>
2021-04-23 08:56:39 -0700

    rename

---
+++ deckzap/asgi.py
@@ -0,0 +1,19 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deckzap.settings')
+django.setup()
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import battle_wizard.routing
+
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            battle_wizard.routing.websocket_urlpatterns
+        )
+    ),
+})
---
+++ deckzap/urls.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.urls import path
+from battle_wizard.views import build_deck
+from battle_wizard.views import create
+from battle_wizard.views import find_custom_game
+from battle_wizard.views import find_game
+from battle_wizard.views import games
+from battle_wizard.views import index
+from battle_wizard.views import manifesto
+from battle_wizard.views import play_custom_game
+from battle_wizard.views import play_game
+from battle_wizard.views import profile
+from battle_wizard.views import save_deck
+from ss.views import dnd
+from ss.views import hangman
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+  	path('', index),
+    path('manifesto', manifesto),
+    path('games', games),
+    path('create', create),
+    path('build_deck', build_deck),
+    path('build_deck/save', save_deck),
+    path('play/custom/<game_id>/<room_code>', play_custom_game),
+    path('play/custom/<game_id>', find_custom_game),
+  	path('play/<ai_type>/<game_type>/<room_code>', play_game),
+    path('play/<ai_type>/<game_type>', find_game),
+    path('ss/hangman', hangman),
+    path('ss/dnd', dnd),
+    path('u/<username>', profile),
+ ]
