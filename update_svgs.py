8e8b9e71349026137a333bba07a9b62ebc8a6e04
Andrew L. Johnson <andrewljohnson@gmail.com>
2021-08-17 11:44:49 -0700

    updated graphics

---
+++ update_svgs.py
@@ -0,0 +1,23 @@
+"""
+	Add height and width attributes to SVGs because pixi.js needs them.
+"""
+
+import os
+import xml.dom.minidom
+
+directory = 'static/images/card-art/'
+
+for filename in os.listdir(directory):
+	full_path = os.path.join(directory, filename)
+	if filename.endswith('svg'):
+		doc = xml.dom.minidom.parse(full_path)
+		svg = doc.getElementsByTagName('svg')[0]
+		svg.attributes['height'] = '96'
+		svg.attributes['width'] = '96'
+		with open(full_path, 'w') as file:
+			file.write(doc.toprettyxml())
+		# minidom adds an xml tag that breaks the svg
+		with open(full_path, 'r') as fin:
+			data = fin.readlines()[1:]
+		with open(full_path, 'w') as fout:
+			fout.writelines(data)
