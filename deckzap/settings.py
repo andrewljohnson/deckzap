577bea1d8850ebf23b0ed4d67077cc5ac50897d5
Andrew L. Johnson <andrewljohnson@gmail.com>
2021-09-30 10:41:42 -0700

    tweak settings

--- deckzap/settings.py
+++ deckzap/settings.py
@@ -128,6 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    "/home/django/deckzap/src/deckzap/staticfiles",
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
