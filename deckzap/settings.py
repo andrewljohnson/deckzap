945b518e4b25569bd9660f4d07cf7d44c33c4c8c
Nathan Cahill <nathan@nathancahill.com>
2021-11-12 22:30:26 -0600

    fix test database name

--- deckzap/settings.py
+++ deckzap/settings.py
@@ -83,10 +83,10 @@ DATABASES = {
         'PASSWORD': os.environ.get("SQL_PASSWORD", "password"),
         'HOST': os.environ.get("SQL_HOST", "localhost"),
         'PORT': os.environ.get("SQL_PORT", "5432"),
+        'TEST': {
+            'NAME': f'test_db_{os.environ.get("GITHUB_RUN_ID", "deckzap")}'
+        },
     },
-    'TEST': {
-        'NAME': f'test_db_{os.environ.get("GITHUB_RUN_ID", "deckzap")}'
-    }
 }
 
 # Password validation
